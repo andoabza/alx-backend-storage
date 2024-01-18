929281db11fb257ebf42f86e996e9204097b35b7
ando <terbu102@gmail.com>
2024-01-18 21:24:30 +0300

    fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- select all bands with if stlye is glam ROck and calculate lifespan until 2022
-
+-- glam rock
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
