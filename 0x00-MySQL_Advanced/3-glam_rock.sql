64dd69557f6a90d25d8355616ccf5cb2ea3bfbde
ando <terbu102@gmail.com>
2024-01-18 09:51:59 +0300

    glab rock bands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- Active: 1705485893796@@127.0.0.1@3306@holberton
 -- glam ROck
 
-SELECT band_name, COALESCE(split, 2022) - formed  AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%';
+SELECT band_name,  ABS(GREATEST(formed, COALESCE(split, 2020)) - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+
