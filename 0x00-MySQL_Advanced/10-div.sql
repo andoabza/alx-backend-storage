ff5134dceffabc56013344064f856f590ebef098
ando <terbu102@gmail.com>
2024-01-19 15:40:28 +0300

    retrun 0

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,6 +4,9 @@ CREATE FUNCTION safeDiv (a INT, b INT)
     RETURNS INT
     DETERMINISTIC
     BEGIN
+        IF b = 0 THEN
+            RETURN 0;
+        END IF;
         RETURN a / b;
     END$$
 DELIMITER ;
