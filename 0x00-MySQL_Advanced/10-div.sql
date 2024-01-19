b5468f1f63c40df9c30b2bdded38d40ba7398755
ando <terbu102@gmail.com>
2024-01-19 15:37:26 +0300

    end if

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,7 +5,11 @@ CREATE FUNCTION safeDiv (a INT, b INT)
     DETERMINISTIC
     BEGIN
         DECLARE result INT;
+        IF b = 0 THEN
+            SET result = 0;
+        ELSE
             SET result = a / b;
+        END IF;
         RETURN result;
     END$$
 DELIMITER ;
