53b15f920c427d69d4082c1476217171bcebccb6
ando <terbu102@gmail.com>
2024-01-19 15:27:38 +0300

    echo "SELECT (a / b) FROM numbers;" | mysql -uroot -p holberton

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,17 @@
 -- create divider FUNCTION
 DELIMITER $$
 CREATE FUNCTION safeDiv (a INT, b INT)
-RETURNS INT
-BEGIN
-    DECLARE result INT;
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result = a / b;
-    END IF;
-    RETURN result;
-END$$
+    RETURNS INT
+    DETERMINISTIC
+    BEGIN
+        DECLARE result INT;
+        IF b = 0 THEN
+            SET result = 0;
+        ELSE
+            SET result = a / b;
+        END IF;
+        RETURN result;
+    END$$
 
 DELIMITER ;
 ```
--- 0x00-MySQL_Advanced/metal_bands.sql
+++ 0x00-MySQL_Advanced/metal_bands.sql
@@ -1,9 +1,13 @@
--- Show and compute average score
-SELECT * FROM users;
-SELECT * FROM corrections;
+DROP TABLE IF EXISTS numbers;
 
-SELECT "--";
-CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+CREATE TABLE IF NOT EXISTS numbers (
+    a int default 0,
+    b int default 0
+);
 
-SELECT "--";
-SELECT * FROM users;
+INSERT INTO numbers (a, b) VALUES (10, 2);
+INSERT INTO numbers (a, b) VALUES (4, 5);
+INSERT INTO numbers (a, b) VALUES (2, 3);
+INSERT INTO numbers (a, b) VALUES (6, 3);
+INSERT INTO numbers (a, b) VALUES (7, 0);
+INSERT INTO numbers (a, b) VALUES (6, 8);
