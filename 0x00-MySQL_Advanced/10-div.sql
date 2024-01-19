ef0226ca86f321109c03bca286d3ff6365c89c94
ando <terbu102@gmail.com>
2024-01-19 15:39:36 +0300

    float

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,13 +4,6 @@ CREATE FUNCTION safeDiv (a INT, b INT)
     RETURNS INT
     DETERMINISTIC
     BEGIN
-        DECLARE result INT;
-        IF b = 0 THEN
-            SET result = 0;
-        ELSE
-            SET result = a / b;
-        END IF;
-        RETURN result;
+        RETURN a / b;
     END$$
 DELIMITER ;
-```
