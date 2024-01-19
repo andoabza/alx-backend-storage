ac5c7ab563681c60ab5221fbe643ac84688567c1
ando <terbu102@gmail.com>
2024-01-19 15:23:53 +0300

    safe civ

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+-- create divider FUNCTION
+DELIMITER $$
+CREATE FUNCTION safeDiv (a INT, b INT)
+RETURNS INT
+BEGIN
+    DECLARE result INT;
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END$$
+
+DELIMITER ;
+```
