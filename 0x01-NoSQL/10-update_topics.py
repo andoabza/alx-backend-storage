f86418d0eebe3f2b5f347728bbda40142555caf2
ando <terbu102@gmail.com>
2024-01-23 16:21:07 +0300

    new  values

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""update a document in a collection based on kwargs"""
+
+
+def update_topics(mongo_collection, name, topics):
+    myquery = { "name": name }
+    newvalues = { "$set": { topics: topics } }
+    return mongo_collection.update(myquery, newvalues)
