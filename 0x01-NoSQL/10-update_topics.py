2a04acf75db0899afb214ae1ddc7f9a700474300
ando <terbu102@gmail.com>
2024-01-23 16:31:25 +0300

    update one

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,5 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     myquery = { "name": name }
-    newvalues = { "$set": { topics: topics } }
-    return mongo_collection.update(myquery, newvalues)
+    newvalues = { "$set": { 'topics': topics } }
+    return mongo_collection.update_one(myquery, newvalues)
