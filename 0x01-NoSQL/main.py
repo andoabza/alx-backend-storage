417dca4729335cb9d706258f439a2b5b757d5b4a
ando <terbu102@gmail.com>
2024-01-23 16:46:22 +0300

    collec6t

--- 0x01-NoSQL/main.py
+++ 0x01-NoSQL/main.py
@@ -5,15 +5,4 @@ update_topics = __import__('10-update_topics').update_topics
 if __name__ == "__main__":
     client = MongoClient('mongodb+srv://mongouser:mongouser@cluster0.vsds4ez.mongodb.net/')
     school_collection = client.my_db.school
-    update_topics(school_collection, "Holberton school", ["Sys admin", "AI", "Algorithm"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-
-    update_topics(school_collection, "Holberton school", ["iOS"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-    client.close()
+    update_topics(school_collection, "ando", ["Sys admin", "AI", "Algorithm"])
