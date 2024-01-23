78f05c5713b0a03684c15e379806bb97922332c7
ando <terbu102@gmail.com>
2024-01-23 18:15:38 +0300

    pycode fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """update a document in a collection based on name"""
 
+
 if __name__ == '__main__':
     def update_topics(mongo_collection, name, topics):
         """update a document in a collection based on name""" 
