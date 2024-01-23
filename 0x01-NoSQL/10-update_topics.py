2682b2ae2e74dfd44f7d1d6b91bedd3f2136020f
ando <terbu102@gmail.com>
2024-01-23 18:11:04 +0300

    update many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""update a document in a collection based on kwargs"""
+"""update a document in a collection based on name"""
 
 if __name__ == '__main__':
-    def update_topics(mongo_collection, name, topics):
-        mongo_collection.update({"name": name},{"$set":{"topics": topics}})
+    def update_topics(mongo_collection, name, topics): 
+        mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
