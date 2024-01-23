0da79810ff825f18f83cef2c19e3a46552bd3356
ando <terbu102@gmail.com>
2024-01-23 18:20:50 +0300

    collection

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,4 +5,7 @@
 if __name__ == '__main__':
     def update_topics(mongo_collection, name, topics):
         """update a document in a collection based on name""" 
-        mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+        mongo_collection.update_many(
+            {"name": name}, 
+            {"$set": {"topics": topics}}
+            )
