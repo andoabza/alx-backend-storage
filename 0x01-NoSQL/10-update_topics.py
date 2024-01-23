e1ebb17e9374ae3319c881a6dc54c9a3610ce542
ando <terbu102@gmail.com>
2024-01-23 18:09:12 +0300

    if name == amain

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """update a document in a collection based on kwargs"""
 
-
-def update_topics(mongo_collection, name, topics):
-    mongo_collection.update({"name": name},{"$set":{"topics": topics}})
+if __name__ == '__main__':
+    def update_topics(mongo_collection, name, topics):
+        mongo_collection.update({"name": name},{"$set":{"topics": topics}})
