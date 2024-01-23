6cbf4e397d43d002909c5dded4fdcdb35acb0eb6
ando <terbu102@gmail.com>
2024-01-23 14:54:48 +0300

    find

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
+"""!/usr/bin/env python3"""
 """connect to mongodb"""
-from pymongo import MongoClient
-
 
 def list_all(mongo_collection):
     """return collection"""
-    client = MongoClient("mongodb://localhost:27017/")
-    school = client.my_db.mongo_collection
-    return school.find()
+    return mongo_collection.find()
