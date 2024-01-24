f2b358b9d1896db8b40937979fc90028f692ee3b
ando <terbu102@gmail.com>
2024-01-24 17:27:46 +0300

    get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,18 +18,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None)\
-            -> Union[str, bytes, int, float]:
+    def get(self, key: str) -> Union[str, bytes, int, float]:
         '''get data from redis'''
-        data = self._redis.get(key)
-        if fn:
-            return fn(data)
         return data
 
-    def get_str(self, key: str) -> str:
-        '''get data from redis as string'''
-        return self.get(key, str)
-
-    def get_int(self, key: str) -> int:
-        '''get data from redis as int'''
-        return self.get(key, int)
+    
