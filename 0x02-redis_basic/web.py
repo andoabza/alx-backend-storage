4a82b95036ceec3f52b96005003887506785529f
ando <terbu102@gmail.com>
2024-01-24 20:39:13 +0300

    response

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 '''obtain content of url'''
 import requests
+import redis
 
 
 def get_page(url: str) -> str:
     '''return content and count'''
-    r = requests.get('http://slowwly.robertomurray.co.uk')
-    return r.content
+    r = redis.Redis()
+    if r.exists(f"cache:{url}"):
+        return r.get(f"cache:{url}")
+    else:
+        response = requests.get(url)
+        r.set(f"cache:{url}", response.text)
+        r.expire(f"cache:{url}", 10)
+        return response.text
+    
