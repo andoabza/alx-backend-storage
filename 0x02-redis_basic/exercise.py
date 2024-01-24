7bc9e33570f5df043009cde9a2207b952b470fcf
ando <terbu102@gmail.com>
2024-01-24 17:24:47 +0300

    store data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
-"""redis basic"""
+'''insert data to store in redis'''
 import redis
 import uuid
+from typing import Union, Callable, Optional
 
 
 class Cache:
-    '''a class that will insert data'''
+    '''class to store data in redis'''
     def __init__(self):
-        '''initialization'''
+        '''constructor'''
         self._redis = redis.Redis()
-        #self._redis.flushdb()
+        self._redis.flushdb()
 
-    def store(self, data):
-        '''save store'''
-        to_byte = bytes(str(uuid.uuid1()), 'utf-8')
-        new_data = bytes(data)
-        self._redis.set(new_data, to_byte)
-        self._redis.set(to_byte, new_data)
-        result = self._redis.get(new_data)
-        self._redis.set(to_byte, new_data)
-        bew = result.decode('utf-8')
-        return str(bew)
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''store data in redis'''
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None)\
+            -> Union[str, bytes, int, float]:
+        '''get data from redis'''
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''get data from redis as string'''
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        '''get data from redis as int'''
+        return self.get(key, int)
