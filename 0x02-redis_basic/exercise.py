c96e58d15965a528de717fc56bbf7d415275503d
ando <terbu102@gmail.com>
2024-01-24 17:20:46 +0300

    save store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,11 +8,16 @@ class Cache:
     '''a class that will insert data'''
     def __init__(self):
         '''initialization'''
-        self._redis = redis.Redis(host='localhost')
-        self._redis.flushdb()
+        self._redis = redis.Redis()
+        #self._redis.flushdb()
 
     def store(self, data):
+        '''save store'''
         to_byte = bytes(str(uuid.uuid1()), 'utf-8')
         new_data = bytes(data)
         self._redis.set(new_data, to_byte)
-        return self._redis.get(new_data)
+        self._redis.set(to_byte, new_data)
+        result = self._redis.get(new_data)
+        self._redis.set(to_byte, new_data)
+        bew = result.decode('utf-8')
+        return bew
