3fbdb49601a4dbd153a74470a0d2d4796037c069
ando <terbu102@gmail.com>
2024-01-24 19:43:33 +0300

    main file

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,12 +1,10 @@
-import redis
-
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
