41aaf36d8735ad33bff2e011796c2d794d7331c7
ando <terbu102@gmail.com>
2024-01-24 16:06:02 +0300

    resid basic

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""redis basic"""
+import redis
+import uuid
+
+class Cache:
+    '''a class that will insert data'''
+    def __init__(self):
+        '''initialization'''
+        self._redis = redis.Redis(host='localhost')
+        self._redis.flushdb()
+
+    def store(self, data):
+        result = self._redis.set(data, uuid.uuid1())
+        return result.get(data)
---
+++ main.py
@@ -0,0 +1,12 @@
+import redis
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
