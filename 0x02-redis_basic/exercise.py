8edd5ef46ea9547e15a65033384dbe2a09008677
ando <terbu102@gmail.com>
2024-01-24 16:24:27 +0300

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,5 +12,7 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data):
-        result = self._redis.set(data, uuid.uuid1())
+        to_byte = bytes(str(uuid.uuid1()), 'utf-8')
+
+        result = self._redis.set(data, to_byte)
         return result.get(data)
